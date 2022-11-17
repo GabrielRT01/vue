7698286d3f0342e0f89df2c30a042652004e761a
Gabriel <gabrirot12@gmail.com>
2022-11-17 11:28:20 +0000

    Modal added

--- resources/js/components/NotFound.vue
+++ resources/js/components/NotFound.vue
@@ -1,3 +1,7 @@
+<script setup>
+    import Home from './Home.vue';
+</script>
+
 <template>
     <div class="container text-center">
         <div class="row g-2">
@@ -11,5 +15,30 @@
                 </router-link>
             </div>
         </div>
+        <!-- Button trigger modal -->
+        <br>
+        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+            ¡Prueba nuestro nuevo modal!
+        </button>
+
+        <!-- Modal -->
+        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Inicio</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <Home></Home>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary">Save changes</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
     </div>
 </template>
